ec80a719f5888e583a31eb41e3401adc60fd5ab6
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:12:33 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,32 @@ output "dns_txt_records" {
   description = "All dns_txt_record resources"
   value       = azurerm_dns_txt_record.dns_txt_records
 }
+output "dns_txt_records_fqdn" {
+  description = "List of fqdn values across all dns_txt_records"
+  value       = [for k, v in azurerm_dns_txt_record.dns_txt_records : v.fqdn]
+}
+output "dns_txt_records_name" {
+  description = "List of name values across all dns_txt_records"
+  value       = [for k, v in azurerm_dns_txt_record.dns_txt_records : v.name]
+}
+output "dns_txt_records_record" {
+  description = "List of record values across all dns_txt_records"
+  value       = [for k, v in azurerm_dns_txt_record.dns_txt_records : v.record]
+}
+output "dns_txt_records_resource_group_name" {
+  description = "List of resource_group_name values across all dns_txt_records"
+  value       = [for k, v in azurerm_dns_txt_record.dns_txt_records : v.resource_group_name]
+}
+output "dns_txt_records_tags" {
+  description = "List of tags values across all dns_txt_records"
+  value       = [for k, v in azurerm_dns_txt_record.dns_txt_records : v.tags]
+}
+output "dns_txt_records_ttl" {
+  description = "List of ttl values across all dns_txt_records"
+  value       = [for k, v in azurerm_dns_txt_record.dns_txt_records : v.ttl]
+}
+output "dns_txt_records_zone_name" {
+  description = "List of zone_name values across all dns_txt_records"
+  value       = [for k, v in azurerm_dns_txt_record.dns_txt_records : v.zone_name]
+}
 
